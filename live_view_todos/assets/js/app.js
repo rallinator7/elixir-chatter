6bce841efd8ebcb4a10193151c73809f070d1cc6
Simon Rall <rallinator7@gmail.com>
2021-06-21 17:35:22 -0500

    Updated to a chat app and added workflow
    s
    
    
    s

--- live_view_todos/assets/js/app.js
+++ live_view_todos/assets/js/app.js
@@ -10,27 +10,33 @@ import "../css/app.scss"
 // Import deps with the dep name or local files with a relative path, for example:
 //
 //     import {Socket} from "phoenix"
-//     import socket from "./socket"
-//
+
+import socket from "./socket"
 import "phoenix_html"
-import {Socket} from "phoenix"
-import topbar from "topbar"
-import {LiveSocket} from "phoenix_live_view"
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let channel = socket.channel('room:lobby', {}); // connect to chat "room"
 
-// Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+channel.on('shout', function (payload) { // listen to the 'shout' event
+  let li = document.createElement("li"); // create new list item DOM element
+  let name = payload.name || 'guest';    // get name from payload or set default
+  li.innerHTML = '<b>' + name + '</b>: ' + payload.message; // set li contents
+  ul.appendChild(li);                    // append to list
+});
 
-// connect if there are any LiveViews on the page
-liveSocket.connect()
+channel.join(); // join the channel.
 
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
 
+let ul = document.getElementById('msg-list');        // list of messages.
+let name = document.getElementById('name');          // name of message sender
+let msg = document.getElementById('msg');            // message input field
+
+// "listen" for the [Enter] keypress event to send a message:
+msg.addEventListener('keypress', function (event) {
+  if (event.keyCode == 13 && msg.value.length > 0) { // don't sent empty msg.
+    channel.push('shout', { // send the message to the server on "shout" channel
+      name: name.value,     // get value of "name" of person sending the message
+      message: msg.value    // get message text (value) from msg input field.
+    });
+    msg.value = '';         // reset the message input field for next message.
+  }
+});
