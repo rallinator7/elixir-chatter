fb4963bed45439741656933d1e1b3f9d19ff594e
Simon Rall <rallinator7@gmail.com>
2021-07-01 14:08:00 -0500

    updated to get dependencies to run mix

--- lib/mix/tasks/cli/ci/ci.unit_test.ex
+++ lib/mix/tasks/cli/ci/ci.unit_test.ex
@@ -3,9 +3,6 @@ defmodule Mix.Tasks.Ci.UnitTest do
 
   @shortdoc "Runs unit tests for project"
   def run(_) do
-    IO.puts("Building Dependencies...")
-    Cli.runCommand("mix", ["deps.get"])
-
     IO.puts("Running Tests...")
     Cli.runCommand("mix", ["test"])
 
